3fe1c6f100d4f3a34b2d6dec16fff0c761e6673f
tamdan <142738725+tamdan44@users.noreply.github.com>
2025-02-27 08:45:36 +0700

    Delete draft.py

--- draft.py
+++
@@ -1,9 +0,0 @@
-import requests
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
-import os
-import json
-import re
-house_number_match = re.findall(r'\b\d+[A-Za-z]?\b', '18a/4 Nguyễn Văn Cừ, P. 4, Q. 5, TP. HCM')
-print(house_number_match)
